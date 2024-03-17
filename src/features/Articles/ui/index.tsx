f044fbfd4087331e3b2dcb23d5f346a7f213e0b1
Dmitriy Menliyakhmatov <d.menliyakhmatov@space307.com>
2024-03-17 12:17:31 +0300

    make template for homework

--- src/features/Articles/ui/index.tsx
+++ src/features/Articles/ui/index.tsx
@@ -1,35 +1,8 @@
 import avatar from 'assets/images/avatar.jpg';
 import coverImg from 'assets/images/cover.webp';
-import { useState } from 'react';
 import { Article } from '../../../shared/types/article';
 import { StatsButtons } from './StatsButtons';
 import s from './post.module.css';
-
-// const postData = {
-//   section: 'Technology',
-//   authorName: 'John Doe',
-//   authorAvatar: 'path-to-avatar.jpg',
-//   publicationDate: 'January 15, 2023',
-//   title: 'Lorem Ipsum Dolor Sit Amet',
-//   coverImage: 'path-to-cover-image.jpg',
-//   views: 1234,
-//   likes: 56,
-//   comments: 23,
-//   bookmarks: 10,
-// };
-
-// const a: Record<string, string | number> = {
-//   age: 29,
-//   name: 'Dmitrii',
-// };
-
-// const obj1 = {
-//   name: 'sads'
-// }
-
-// const obj2 = {
-//   link: obj1
-// }
 
 type PostCardProps = {
   postData: Article;
@@ -37,15 +10,7 @@ type PostCardProps = {
   // two: string;
 };
 
-// export const PostCard = (props: PostCardProps) => {
 export const PostCard = (props: PostCardProps) => {
-  // const postData = props.postData;
-  // const one = props.one
-  // const two = props.two
-  // const { one, postData, two } = props;
-
-  const [titleVisibilty, setTitleVisiblity] = useState(true);
-
   const { postData } = props;
 
   return (
@@ -64,15 +29,8 @@ export const PostCard = (props: PostCardProps) => {
         </div>
       </div>
 
-      {titleVisibilty && <h2>{postData.title}</h2>}
+      <h2>{postData.title}</h2>
 
-      <button
-        onClick={() => {
-          setTitleVisiblity(!titleVisibilty);
-        }}
-      >
-        {titleVisibilty ? 'Hide title' : 'Show title'}
-      </button>
       <div className={s.coverImage}>
         <img src={coverImg} alt="cover" />
       </div>
@@ -81,16 +39,3 @@ export const PostCard = (props: PostCardProps) => {
     </div>
   );
 };
-
-// const div = () => {};
-
-// let a = 'string'
-
-// const func = (options: {name: string, }) => {
-
-// const newOptions = {...options}
-
-// a = 'another string '
-// options.name = 'some name'
-
-// };
