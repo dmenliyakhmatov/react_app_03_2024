5eed2e1345b145339beb443bb6b6fa5fe879ca04
Dmitriy Menliyakhmatov <d.menliyakhmatov@space307.com>
2024-04-06 10:34:17 +0300

    useEffect examples

--- src/pages/ResizeExample.tsx
+++ src/pages/ResizeExample.tsx
@@ -1,7 +1,89 @@
+import { useEffect, useState } from 'react';
+
 export const ResizeExamplePage = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resizeHandler = () => {
+      console.log('call');
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', resizeHandler);
+
+    return () => window.removeEventListener('resize', resizeHandler);
+  }, []);
+
   return (
     <div>
       <h2>Resize example</h2>
+
+      <h3>width {windowWidth}</h3>
     </div>
   );
 };
+//==================================
+
+//==================================
+//==================================
+//==================================
+const user1 = {
+  id: 1,
+  name: 'Dmitrii',
+  age: 29,
+  friends: [
+    { id: 2, name: 'Maksim', age: 23 },
+    { id: 3, name: 'Aleksei', age: 23 },
+  ],
+};
+
+const user2 = user1;
+
+// user2.age = 30;
+
+const updateName = (user: typeof user1, age: number) => {
+  const newUser = { ...user, age };
+
+  //   newUser.age = age
+
+  return newUser;
+};
+
+const addFriend = (user: typeof user1, name: string) => {
+  const newUser = { ...user, friends: [...user.friends, { name, age: 34, id: 4 }] };
+
+  return newUser;
+};
+
+const user3 = updateName(user1, 30);
+
+const user4 = addFriend(user1, 'Kirill');
+
+console.log('user1', user1);
+console.log('user2', user2, user1 === user2);
+
+console.log('user3', user3, user1.friends === user3.friends);
+console.log('user4', user4);
+
+const state = {
+  component1: {
+    couter: 1,
+  },
+  component2: {
+    articleList: [{ id: 1 }, { id: 2 }],
+  },
+};
+
+const prevState = {
+  component1: {
+    couter: 1,
+    articleList: [{ id: 1 }, { id: 2 }],
+  },
+  component2: {
+    articleList: [{ id: 1 }, { id: 2 }],
+  },
+};
+
+const newState = { ...state, component1: { ...state.component1, counter: 3 } };
+
+// prevState.component1 === newState.component1;
--- src/pages/TimerPage.tsx
+++ src/pages/TimerPage.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from 'react';
+
 export const TimerPage = () => {
+  const [seconds, setSeconds] = useState(0);
+  const [isVisibleGreeting, setIsVisibleGreeting] = useState(true);
+
+  useEffect(() => {
+    console.log('timer has mounted');
+
+    const timerId = setInterval(() => {
+      console.log('timer has called');
+      setSeconds(prev => prev + 1);
+    }, 1000);
+
+    return () => {
+      console.log('timer has unmounted');
+      clearInterval(timerId);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (seconds > 5) setIsVisibleGreeting(false);
+  }, [seconds]);
+
   return (
     <div>
       <h2>Timer</h2>
+      {isVisibleGreeting && <h3>Hello, effect</h3>}
+      <p>Second: {seconds}</p>
     </div>
   );
 };
